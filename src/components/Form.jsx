4477585980f2166eb7ff7eba1ad107c0a206b286
Phyllipe Bezerra <pmba@ic.ufal.br>
2022-01-17 18:02:13 -0300

    Adiciona descrição do projeto e badge de fork

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 import Button from "@mui/material/Button";
-import { Stack, TextField } from "@mui/material";
+import { Divider, Stack, TextField, Typography } from "@mui/material";
 import { pushNewItem } from "../services/database";
 
 const Form = () => {
@@ -41,6 +41,23 @@ const Form = () => {
 
     return (
         <Stack spacing={2}>
+            <Typography variant="h5" component="div">
+                Sistemas Distribuídos 2021.1
+            </Typography>
+            <Typography
+                variant="body2"
+                color="text.secondary"
+                style={{
+                    marginBottom: "1rem",
+                }}
+            >
+                Projeto feito para a disciplina de sistemas distribuídos da
+                Universidade Federal de Alagoas, com o intuito de mostrar um
+                exemplo prático de utilização do Firebase Realtime Database.
+                Nesse exemplo podemos adicionar ou remover itens ao banco de
+                dados e visualizar as inserções e remoções em tempo real.
+            </Typography>
+
             <TextField
                 id="outlined-basic"
                 label="Título"
--- src/index.js
+++ src/index.js
@@ -7,6 +7,22 @@ import "./styles.css";
 
 ReactDOM.render(
     <React.StrictMode>
+        <a
+            href="https://github.com/pmba/sd-realtime-database"
+            className="fork-me-badge"
+            target="_blank"
+            rel="noreferrer"
+        >
+            <img
+                loading="lazy"
+                width="149"
+                height="149"
+                src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149"
+                class="attachment-full size-full"
+                alt="Fork me on GitHub"
+                data-recalc-dims="1"
+            />
+        </a>
         <HomeScreen />
     </React.StrictMode>,
     document.getElementById("root")
